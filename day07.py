da05416e5b60aad72e9a59bdda09327cd8658c44
Dimitris Hol <dimitrishol@protonmail.com>
2021-12-22 18:08:18 +0200

    Day07 refactoring and optimizing

--- day07.py
+++ day07.py
@@ -1,8 +1,8 @@
 '''
 You must find a position, which they can all horizontally align
-and that's it's gonna require the least fuel to do so (in total)
+and that's it's gonna require the least int(fuel) to do so (in total)
 '''
-def part1() : 
+def solve() : 
     with open("input/day07.txt") as file : 
 
         for line in file : 
@@ -16,28 +16,36 @@ def part1() :
             for i in range(len(positions)) : 
                 counts[positions[i]] = positions.count(positions[i])
 
-            print(positions)
-            print(counts)
-
-            fuelRequirements = [1000000000000, None]
+            minimumFuelRequiredPart1 = 1000000000000
+            minimumFuelRequiredPart2 = 1000000000000
 
             for i in range(min(positions), max(positions) + 1) : 
-                fuelRequired = 0
+                fuelRequiredPart1 = 0
+                fuelRequiredPart2 = 0
 
                 # Calculate the distance for each position
                 # i = 0 , how much fuel to transfer everyone to 0
 
                 for key in counts.keys() : 
-                    distance = abs(i - key) # 
-                    part2Consumption = sum([i for i in range(distance+1)])
+                    distance = abs(i - key) 
 
-                    fuelRequired += distance * counts[key] # Part 1
-                    fuelRequired += part2Consumption * counts[key]  # Part 2 , kinda slow
+                    # part2Consumption = sum([i for i in range(distance+1)])  # This is really really slow 
+                    # https://en.wikipedia.org/wiki/Triangular_number
+                    
+                    # You can calculate it really fast with this : 
+                    part2Consumption = (distance * (distance + 1)) / 2
+
+                    fuelRequiredPart1 += distance * counts[key] # Part 1
+                    fuelRequiredPart2 += part2Consumption * counts[key]  # Part 2
                 
-                if fuelRequired < fuelRequirements[0] : 
-                    fuelRequirements[0] = fuelRequired
-                    fuelRequirements[1] = i
+                if fuelRequiredPart1 < minimumFuelRequiredPart1 : 
+                    minimumFuelRequiredPart1 = fuelRequiredPart1
 
-            print(fuelRequirements)
+                minimumFuelRequiredPart1 = min(minimumFuelRequiredPart1, fuelRequiredPart1)
+                minimumFuelRequiredPart2 = min(minimumFuelRequiredPart2, fuelRequiredPart2)
 
-part1()
+
+            print("Part 1 :", minimumFuelRequiredPart1)
+            print("Part 2 :", int(minimumFuelRequiredPart2))
+
+solve()
